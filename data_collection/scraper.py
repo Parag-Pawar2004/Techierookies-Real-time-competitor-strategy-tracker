e69e668ae8f8d6fc63af3888310d7124380365eb
Chaturiya <chaturiya06@gamil.com>
2025-09-25 15:09:32 +0530

    Day2: Added dummy scraper script for iphone model

---
+++ data_collection/scraper.py
@@ -0,0 +1,16 @@
+import datetime
+def scrape_iphone(model_name):
+    """
+    Dummy scraper: returns fake data for now.
+    Later we will replace with CrawlAI
+    """
+    data = {
+        "model": model_name,
+        "price": "80,000",
+        "site":"Amazon",
+        "scraped_at": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    }
+    return data
+if __name__=="__main__":
+    iphone = scrape_iphone("iPhone 15 Pro")
+    print(iphone)
